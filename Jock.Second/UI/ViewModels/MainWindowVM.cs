4063a18ceee659c1061430974c95924ff956377c
Unknown <Peter96pvl@gmail.com>
2019-09-25 14:18:40 +0700

    #002
    Правки.

--- Jock.Second/UI/ViewModels/MainWindowVM.cs
+++ Jock.Second/UI/ViewModels/MainWindowVM.cs
@@ -19,6 +19,94 @@
             GetEdgesCommand = new GetEdgesCommand();
         }
 
+        #region Выборы методов
+
+        /// <summary>
+        /// Выбор метода Лапласса.
+        /// </summary>
+        private bool _isLaplass;
+
+        /// <summary>
+        /// Выбор метода Лапласса.
+        /// </summary>
+        public bool IsLaplass
+        {
+            get => _isLaplass;
+            set
+            {
+                _isLaplass = value;
+
+                if (value == true)
+                {
+                    _isRoberts = false;
+                    OnPropertyChanged(nameof(IsRoberts));
+
+                    _isSobel = false;
+                    OnPropertyChanged(nameof(IsSobel));
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Выбор метода Робертса.
+        /// </summary>
+        private bool _isRoberts;
+
+        /// <summary>
+        /// Выбор метода Робертса.
+        /// </summary>
+        public bool IsRoberts
+        {
+            get => _isRoberts;
+            set
+            {
+                _isRoberts = value;
+
+                if (value == true)
+                {
+                    _isLaplass = false;
+                    OnPropertyChanged(nameof(IsLaplass));
+
+                    _isSobel = false;
+                    OnPropertyChanged(nameof(IsSobel));
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Выбор метода Собеля.
+        /// </summary>
+        private bool _isSobel;
+
+        /// <summary>
+        /// Выбор метода Собеля.
+        /// </summary>
+        public bool IsSobel
+        {
+            get => _isSobel;
+            set
+            {
+                _isSobel = value;
+
+                if (value == true)
+                {
+                    _isLaplass = false;
+                    OnPropertyChanged(nameof(IsLaplass));
+
+                    _isRoberts = false;
+                    OnPropertyChanged(nameof(IsRoberts));
+                }
+
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
         #region Данные изображения.
 
         /// <summary>
